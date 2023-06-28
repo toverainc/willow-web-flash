b420794107a58e7c3dcf0ea50b1898a15e3a5cb2
Stijn Tintel <stijn@linux-ipv6.be>
2023-06-28 21:07:50 +0300

    Point to willow-dist.bin test release on GH
    For testing, enable no-cors.

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ function ui8ToBstr(u8Array) {
 
 willowSettings.onsubmit = async (event) => {
   event.preventDefault()
-  const buffer = await (await fetch('willow-dist.bin')).arrayBuffer() //XXX: change url
+  const buffer = await (await fetch('https://github.com/toverainc/willow-test/releases/download/0.1.0-alpha1/willow-dist.bin', { mode: "no-cors", redirect: "follow" })).arrayBuffer() //XXX: change url
   const firmware = new Uint8Array(buffer)
 
   const rows = [
