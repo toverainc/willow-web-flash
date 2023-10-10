toverainc/willow-web-flash

commit 85f37667af2f98ef9d383407923dd2fb962307df

index.js: add URL param to show pre-releases

index.js
@@ -50,6 +50,7 @@ const lsWasUrl = localStorage.getItem('wasUrl');
 // Get WAS URL Param (prefer local storage)
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
+const showPreReleases = urlParams.get('showPreReleases');
 const wasURL = urlParams.get('wasURL')
 
 if (lsWasUrl) {
@@ -106,7 +107,7 @@ function updateReleaseDropdown() {
   const num_non_prereleases = deviceReleases.reduce((acc, cur) => cur.prerelease === false ? acc + 1 : acc, 0);
   for (const r of deviceReleases) {
     // skip pre-releases unless there are only pre-releases
-    if (r['prerelease'] == true && num_non_prereleases > 0) {
+    if (r['prerelease'] == true && num_non_prereleases > 0 && !showPreReleases) {
       continue;
     }
     const option = new Option(r['version']);

commit 570227c609407e7c67b02a49218d468aa46c546a

index.js: properly check showPreReleases
Fixes: 85f37667af2f ("index.js: add URL param to show pre-releases")

index.js
@@ -107,7 +107,7 @@ function updateReleaseDropdown() {
   const num_non_prereleases = deviceReleases.reduce((acc, cur) => cur.prerelease === false ? acc + 1 : acc, 0);
   for (const r of deviceReleases) {
     // skip pre-releases unless there are only pre-releases
-    if (r['prerelease'] == true && num_non_prereleases > 0 && !showPreReleases) {
+    if (r['prerelease'] == true && num_non_prereleases > 0 && showPreReleases != "true") {
       continue;
     }
     const option = new Option(r['version']);